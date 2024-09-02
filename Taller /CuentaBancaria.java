07d905de594601e91d626c53dc27e5ee080bd740
Labrador09 <167787491+Labrador09@users.noreply.github.com>
2024-09-02 15:31:12 +0000

    taller

---
+++ Taller /CuentaBancaria.java
@@ -0,0 +1,39 @@
+
+public class CuentaBancaria  {
+
+    private String numeroCuenta;
+    private double saldo;
+    private String titular;
+
+    public CuentaBancaria (String numeroCuenta,String titular, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.titular = titular;
+    }
+    public void depositar(double cantidad) {
+        if (cantidad > 0){
+          saldo += cantidad;
+          System.out.println("tu nuevo saldo es "+ saldo);
+        } else {
+            System.out.println("La cantidad a deporsitar debe ser mayor a 0");
+        }
+      
+    }
+    public void retirar(double cantidad){
+        if (cantidad > 0 && cantidad <= saldo){
+            saldo -= cantidad;
+            System.out.println("Retiraste: "+ cantidad + " Tu nuevo saldo es: "+ saldo);
+        } else {
+            System.out.println("Fondos insuficientes o invalido");
+        }
+    }
+    
+    
+}
+
+
+
+
+
+
+   
---
+++ Taller /Ejercicio1.java
@@ -0,0 +1,9 @@
+public class Ejercicio1 {
+    public static void main(String[]args){
+        CuentaBancaria objCuenta1 = new CuentaBancaria ("1234567", "Miguel", 500.0 );
+        objCuenta1.depositar(100.0);
+        objCuenta1.retirar(100.0);
+        
+
+    }
+}
